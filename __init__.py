534288a0c7a9bc764537e89982e186172e7459c0
shaoqin2 <shaoqin2@illinois.edu>
2016-10-05 19:52:47 -0500

    first meeting

--- __init__.py
+++ __init__.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
-@app.route("/")
-def hello():
-	return "zongzi memeda"
+
 if __name__ == "__main__":
     app.run()
+@app.route("/")
+def hello():
+	return "this bad boy is working!"
+@app.route("/getWord", methods=['GET'])
+def getWord():
+	if 'long' in request.args and 'lat' in request.args:
+		longitude = float(request.args["long"])
+		latitude = float(request.args["lat"])
+		return str(nearest(longitude, latitude))
+	else:
+		return "bad request"
+stuff = []
+stuff.append([0.0,0.0])
+stuff.append([13.0,13.0])
+
+def nearest(longitude,latitude):
+	min = 99999999
+	minPos = -1
+	for i in xrange(len(stuff)):
+		if min > ((stuff[i][0]-longitude)**2 + (stuff[i][1]-latitude)**2):
+			min = ((stuff[i][0]-longitude)**2 + (stuff[i][1]-latitude)**2)
+			minPos=i
+	return stuff[minPos]
+
